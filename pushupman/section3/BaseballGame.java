2c941085d4199efb47853d1e560c33a2189c4274
kyd <husheart@naver.com>
2021-02-18 00:46:41 +0900

    BaseballGame
    Queue & Stack

---
+++ pushupman/section3/BaseballGame.java
@@ -0,0 +1,58 @@
+package pushupman.section3;
+
+import java.util.Stack;
+
+public class BaseballGame {
+
+    private static final String Substract = "C";
+    private static final String Double = "D";
+    private static final String Plus = "+";
+
+    public static void main(String[] args) {
+
+        String[] input = {"5", "-2", "4", "C", "D", "9", "+", "+"};
+
+        int myResult = mySolve(input);
+
+        System.out.println("myResult = " + myResult);
+
+    }
+
+    public static int mySolve(String[] input){
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : input) {
+            switch (s) {
+                case Substract:
+                    stack.pop();
+                    break;
+
+                case Double:
+                    Integer peek = stack.peek();
+                    stack.add(peek * 2);
+                    break;
+
+                case Plus:
+                    Integer lastOne = stack.pop();
+                    Integer secondOne = stack.peek();
+
+                    stack.add(lastOne);
+                    stack.add(lastOne + secondOne);
+                    break;
+
+                default:
+                    stack.add(Integer.parseInt(s));
+            }
+        }
+
+        int result = 0;
+        for (Integer integer : stack) {
+            result += integer;
+        }
+
+        return result;
+
+    }
+
+}
